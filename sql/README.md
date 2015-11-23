8e268e95f143368f8d24e4fdd21ab733b2d2f76f
Alejandro <alejandro.sanchez.cabana@gmail.com>
2015-11-23 11:31:40 +0100

    Initial commit

---
+++ sql/README.md
@@ -0,0 +1,12 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1348\cocoasubrtf170
+{\fonttbl\f0\fnil\fcharset0 Consolas;}
+{\colortbl;\red255\green255\blue255;\red189\green54\blue19;\red83\green104\blue112;}
+\paperw11905\paperh16837\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl380
+
+\f0\fs28 \cf2 \expnd0\expndtw0\kerning0
+# Beeter\cf3 \
+_**Beeter**_ is an educational project developed at course "Dise\'f1o de Servicios y Aplicaciones" (Services and Applications Design) of\
+ Castelldefels School of Telecommunications and Aerospace Engineering (EETAC).\kerning1\expnd0\expndtw0 \
+}
---
+++ sql/where-schema.sql
@@ -0,0 +1,53 @@
+drop database if exists wheredb;
+create database wheredb;
+
+use wheredb;
+
+CREATE TABLE users (
+    id BINARY(16) NOT NULL,
+    loginid VARCHAR(15) NOT NULL UNIQUE,
+    password BINARY(16) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    fullname VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE user_roles (
+    userid BINARY(16) NOT NULL,
+    role ENUM ('registered','admin'),
+    FOREIGN KEY (userid) REFERENCES users (id) on delete cascade,
+    PRIMARY KEY (userid, role)
+);
+
+CREATE TABLE auth_tokens (
+    userid BINARY(16) NOT NULL,
+    token BINARY(16) NOT NULL,
+    FOREIGN KEY (userid) REFERENCES users (id) on delete cascade,
+    PRIMARY KEY (token)
+);
+
+CREATE TABLE restaurants (
+    id BINARY(16) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    description VARCHAR(500) NOT NULL,
+    avgprice DECIMAL NOT NULL,
+    likes INT NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    phone VARCHAR(9) NOT NULL,
+    lat FLOAT( 10, 6 ) NOT NULL,
+    lng FLOAT( 10, 6 ) NOT NULL
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE comments(
+    id BINARY(16) NOT NULL,
+    restaurant BINARY(16) NOT NULL,
+    creator BINARY(16) NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    comment VARCHAR(500) NOT NULL,
+    last_modified TIMESTAMP NOT NULL,
+    creation_timestamp DATETIME not null default current_timestamp,
+    FOREIGN Key (restaurant) REFERENCES restaurants(id) on delete cascade,
+    FOREIGN KEY (creator) REFERENCES users (id) on delete cascade,
+    PRIMARY KEY (id)
+);
