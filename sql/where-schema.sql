d906184280caa9aac0d93419f47f70a4b62da0f9
Alejandro <alejandro.sanchez.cabana@gmail.com>
2015-11-30 12:25:32 +0100

    User Resource add

--- sql/where-schema.sql
+++ sql/where-schema.sql
@@ -15,14 +15,14 @@ CREATE TABLE users (
 CREATE TABLE user_roles (
     userid BINARY(16) NOT NULL,
     role ENUM ('registered','admin','owner'),
-    FOREIGN KEY (userid) REFERENCES users (id) on delete cascade,
+    FOREIGN KEY (userid) REFERENCES users(id) on delete cascade,
     PRIMARY KEY (userid, role)
 );
 
 CREATE TABLE auth_tokens (
     userid BINARY(16) NOT NULL,
     token BINARY(16) NOT NULL,
-    FOREIGN KEY (userid) REFERENCES users (id) on delete cascade,
+    FOREIGN KEY (userid) REFERENCES users(id) on delete cascade,
     PRIMARY KEY (token)
 );
 
@@ -36,7 +36,7 @@ CREATE TABLE restaurants (
     address VARCHAR(100) NOT NULL,
     phone VARCHAR(9) NOT NULL,
     lat FLOAT( 10, 6 ) NOT NULL,
-    lng FLOAT( 10, 6 ) NOT NULL
+    lng FLOAT( 10, 6 ) NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -48,15 +48,15 @@ CREATE TABLE comments(
     comment VARCHAR(500) NOT NULL,
     response VARCHAR(500) NOT NULL,
     creation_timestamp DATETIME not null default current_timestamp,
-    FOREIGN KEY (creator) REFERENCES users (id) on delete cascade,
-    FOREIGN KEY (restaurant) REFERENCES restaurants (id) on delete cascade,
+    FOREIGN KEY (creator) REFERENCES users(id) on delete cascade,
+    FOREIGN KEY (restaurant) REFERENCES restaurants(id) on delete cascade,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE like_restaurant(
     restaurant BINARY(16) NOT NULL,
     user BINARY(16) NOT NULL,
-    FOREIGN KEY (user) REFERENCES user(id) on delete cascade,
+    FOREIGN KEY (user) REFERENCES users(id) on delete cascade,
     FOREIGN KEY (restaurant) REFERENCES restaurants(id) on delete cascade,
     PRIMARY KEY (restaurant, user)
 );
