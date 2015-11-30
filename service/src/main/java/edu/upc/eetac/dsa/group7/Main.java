84322dd22245f0cb6671d4457a14995427062fb5
Alejandro <alejandro.sanchez.cabana@gmail.com>
2015-11-30 11:38:03 +0100

    Main.java fixed

--- service/src/main/java/edu/upc/eetac/dsa/group7/Main.java
+++ service/src/main/java/edu/upc/eetac/dsa/group7/Main.java
@@ -6,6 +6,7 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 import java.io.IOException;
 import java.net.URI;
+import java.sql.SQLException;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 
@@ -14,6 +15,8 @@ import java.util.ResourceBundle;
  *
  */
 public class Main {
+
+
     // Base URI the Grizzly HTTP server will listen on
     private static String baseURI;
 
@@ -31,23 +34,31 @@ public class Main {
      */
     public static HttpServer startServer() {
         // create a resource config that scans for JAX-RS resources and providers
-        // in edu.upc.eetac.dsa.where package
+        // in edu.upc.eetac.dsa.grouptalk package
         final ResourceConfig rc = new WhereResourceConfig();
 
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(getBaseURI()), rc);
     }
-
     /**
      * Main method.
      * @param args
      * @throws IOException
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SQLException {
         final HttpServer server = startServer();
+
+        /*//Admin user creator
+        UserDAO userDAO = new UserDAOImpl();
+        User user = null;
+        AuthToken authenticationToken = null;
+        user = userDAO.createAdmin();
+        authenticationToken = (new AuthTokenDAOImpl()).createAuthToken(user.getId());
+        System.out.println("Admin User created");*/
+
         System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", getBaseURI()));
+                + "%sapplication.wadl\nHit enter to stop it...", baseURI));
         System.in.read();
         server.shutdownNow();
     }
