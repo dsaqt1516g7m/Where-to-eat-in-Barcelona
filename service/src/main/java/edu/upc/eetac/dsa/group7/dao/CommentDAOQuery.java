ee644276beb10c58304d41d0f21aca2fdd685bc8
Alejandro <alejandro.sanchez.cabana@gmail.com>
2015-12-09 09:55:37 +0100

    pagination added

--- service/src/main/java/edu/upc/eetac/dsa/group7/dao/CommentDAOQuery.java
+++ service/src/main/java/edu/upc/eetac/dsa/group7/dao/CommentDAOQuery.java
@@ -7,7 +7,7 @@ public interface CommentDAOQuery {
     public final static String UUID = "select REPLACE(UUID(),'-','')";
     public final static String CREATE_COMMENT = "insert into comments (id, creator, restaurant, title, comment, likes, response) values (UNHEX(?), UNHEX(?), UNHEX(?), ?, ?, ?, ?)";
     public final static String GET_COMMENT_BY_ID = "select hex(id) as id, hex(creator) as creator, hex(restaurant) as restaurant, title, comment, likes, response, creation_timestamp from comments where id=unhex(?)";
-    public final static String GET_COMMENTS = "select hex(id) as id, hex(creator) as creator, hex(restaurant) as restaurant, title, comment, likes, response, creation_timestamp from comments";
+    public final static String GET_COMMENTS = "select hex(id) as id, hex(creator) as creator, hex(restaurant) as restaurant, title, comment, likes, response, creation_timestamp from comments order by creation_timestamp limit 5";
     public final static String RESPONSE_COMMENT = "update comments set response=? where id=unhex(?) ";
     public final static String DELETE_COMMENT = "delete from comments where id=unhex(?)";
     public final static String VALORATION = "insert into like_restaurant (restaurant, user) values (UNHEX(?), UNHEX(?))";
