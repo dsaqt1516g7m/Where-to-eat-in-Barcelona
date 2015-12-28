848644bac9381765351517d99bdf0610098f13a9
Alejandro <alejandro.sanchez.cabana@gmail.com>
2015-12-28 21:02:35 +0100

    Prepare service to run in linuxVM

--- service/src/main/java/edu/upc/eetac/dsa/group7/Main.java
+++ service/src/main/java/edu/upc/eetac/dsa/group7/Main.java
@@ -51,31 +51,8 @@ public class Main {
      * @param args
      * @throws IOException
      */
-    public static void main(String[] args) throws IOException, SQLException {
+    public static void main(String[] args) throws IOException {
         final HttpServer server = startServer();
-/*
-        Methods for creating a admin and owner users (actually not in use)
-
-        //Admin user creator
-        UserDAO userDAO = new UserDAOImpl();
-        User user = null;
-        AuthToken authenticationToken = null;
-        user = userDAO.createAdmin();
-        authenticationToken = (new AuthTokenDAOImpl()).createAuthToken(user.getId());
-        System.out.println("Admin User created");
-
-        //Owner user creator
-        UserDAO userDAO2 = new UserDAOImpl();
-        User user2 = null;
-        AuthToken authenticationToken2 = null;
-        user2 = userDAO2.createOwner();
-        authenticationToken2 = (new AuthTokenDAOImpl()).createAuthToken(user.getId());
-        System.out.println("Owner User created");
-*/
-        System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", baseURI));
-        System.in.read();
-        server.shutdownNow();
     }
 }
 
