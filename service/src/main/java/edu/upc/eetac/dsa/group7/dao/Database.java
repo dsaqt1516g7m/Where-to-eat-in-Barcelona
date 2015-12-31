cb4323f4484170aa004e4c73faade1e6014fee0a
Alejandro <alejandro.sanchez.cabana@gmail.com>
2015-12-31 19:16:58 +0100

    older hikari

--- service/src/main/java/edu/upc/eetac/dsa/group7/dao/Database.java
+++ service/src/main/java/edu/upc/eetac/dsa/group7/dao/Database.java
@@ -6,26 +6,13 @@ import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Enumeration;
-import java.util.Properties;
-import java.util.PropertyResourceBundle;
-import java.util.ResourceBundle;
 
 public class Database {
     private static Database instance = null;
     private DataSource ds;
 
     private Database() {
-        PropertyResourceBundle prb = (PropertyResourceBundle) ResourceBundle.getBundle("hikari");
-        Enumeration<String> keys = prb.getKeys();
-        Properties properties = new Properties();
-        while(keys.hasMoreElements()){
-            String key = keys.nextElement();
-            properties.setProperty(key, prb.getString(key));
-        }
-
-        //HikariConfig config = new HikariConfig(Database.class.getClassLoader().getResource("hikari.properties").getFile());
-        HikariConfig config = new HikariConfig(properties);
+        HikariConfig config = new HikariConfig(Database.class.getClassLoader().getResource("hikari.properties").getFile());
         ds = new HikariDataSource(config);
     }
 
