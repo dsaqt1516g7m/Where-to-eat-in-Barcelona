1145317905def2c6b9896f0c49a4ac46e636776f
Alejandro <alejandro.sanchez.cabana@gmail.com>
2016-01-19 11:53:59 +0100

    errors handling geolocation

--- web/js/admin.js
+++ web/js/admin.js
@@ -46,7 +46,7 @@ $("#create_boton").click(function (e) {
 
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
     } else { 
         alert("Geolocation is not supported by this browser.");
     }
@@ -55,4 +55,21 @@ function getLocation() {
 function showPosition(position) {
     $("#lat").val(position.coords.latitude);
     $("#lng").val(position.coords.longitude);
+}
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            alert("The request to get user location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+           alert("An unknown error occurred.");
+            break;
+    }
 }
